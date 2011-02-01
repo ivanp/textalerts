ab721af1463e6fc2914c679d2df775c5e823f433
ivanp <ivan@primaguna.com>
2011-02-01 10:07:01 +0700

    Another URL issues

--- protected/views/front/site/index.php
+++ protected/views/front/site/index.php
@@ -8,7 +8,7 @@
 
 <div id="media">
             <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="496" height="480" id="csSWF">
-                <param name="movie" value="media/player.swf" />
+                <param name="movie" value="/media/player.swf" />
                 <param name="quality" value="best" />
                 <param name="bgcolor" value="#1a1a1a" />
                 <param name="allowfullscreen" value="true" />
@@ -35,14 +35,14 @@
         <!-- Users looking for simple object / embed tags can copy and paste the needed tags below.
         <div id="media">
             <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="496" height="480" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,115,0">
-                <param name="src" value="media/player.swf"/>
+                <param name="src" value="/media/player.swf"/>
                 <param name="bgcolor" value="#1a1a1a"/>
                 <param name="quality" value="best"/>
                 <param name="allowScriptAccess" value="always"/>
                 <param name="allowFullScreen" value="true"/>
                 <param name="scale" value="showall"/>
                 <param name="flashVars" value="thumb=media/FirstFrame.png&containerwidth=978&containerheight=947&content=myalerts.mp4&autostart=false&blurover=false&autohide=true&smoothing=true&showbranding=false&showstartscreen=true"/>
-                <embed name="csSWF" src="media/player.swf" width="496" height="480" bgcolor="#1a1a1a" quality="best" allowScriptAccess="always" allowFullScreen="true" scale="showall" flashVars="thumb=media/FirstFrame.png&containerwidth=978&containerheight=947&content=myalerts.mp4&autostart=false&blurover=false&autohide=true&smoothing=true&showbranding=false&showstartscreen=true&color=0x1A1A1A,0x1A1A1A" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"></embed>
+                <embed name="csSWF" src="/media/player.swf" width="496" height="480" bgcolor="#1a1a1a" quality="best" allowScriptAccess="always" allowFullScreen="true" scale="showall" flashVars="thumb=media/FirstFrame.png&containerwidth=978&containerheight=947&content=myalerts.mp4&autostart=false&blurover=false&autohide=true&smoothing=true&showbranding=false&showstartscreen=true&color=0x1A1A1A,0x1A1A1A" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"></embed>
             </object>
         </div>
         -->
