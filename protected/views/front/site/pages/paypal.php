0f2bd5b03ad0ac894ced58b82e22cd16ab67294b
ivanp <ivan@primaguna.com>
2011-02-01 09:31:36 +0700

    New pages

---
+++ protected/views/front/site/pages/paypal.php
@@ -0,0 +1,20 @@
+<div style="width: 100%; text-align: center">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="C2G4XUKCJDP2A">
+<table>
+<tr><td><input type="hidden" name="on0" value="Get Started Now!"><b>Get Started Now!</b></td></tr><tr><td><select name="os0">
+        <option value="12 Months (Best Value)">12 Months (Best Value) $250.00</option>
+        <option value="9 Months">9 Months $225.00</option>
+        <option value="6 Months">6 Months $150.00</option>
+        <option value="3 Months">3 Months $75.00</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="USD">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+
+
+</div>
