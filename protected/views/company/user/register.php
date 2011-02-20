6e81d87b000c631ce1d64c8b9de2b2561383801b
Ivan <ivan@primaguna.com>
2011-02-20 11:52:43 -0600

    Changes from Fred

--- protected/views/company/user/register.php
+++ protected/views/company/user/register.php
@@ -8,7 +8,7 @@ $this->breadcrumbs=array(
 
 <h1>Registration</h1>
 
-<p>If you would like to begin receiving email or text messages to advise you of school delays and closings, please enter your first name, last name and email address. After you enter your information, we will send you an email with your password. Once you have a password you may use it to login to the site and then register your cell phone to receive text messages. </p>
+<p>If you would like to begin receiving email or text messages, please enter your first name, last name and email address. After you enter your information, we will send you an email with your password. Once you have a password you may use it to login to the site and then register your cell phone to receive text messages. </p>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
 
